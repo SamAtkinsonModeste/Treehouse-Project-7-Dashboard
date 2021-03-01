d27f3e6a9c9ab1a7ba6dd037eb564e2baa3ff4cd
Sam Atkinson <sammodeste@yahoo.com>
2021-03-01 18:03:51 +0000

    added toggle text on button for color themes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-
+const btn = document.querySelector(".switch");
 
 
 
 document.querySelector('[data-switch-dark]').addEventListener('click', function() {
     document.body.classList.toggle('dark');
+    
+    
+       if (btn.textContent === "Dark Theme") {
+         
+            btn.textContent = "Light Theme";
+       } else {
+            btn.textContent = "Dark Theme";
+       }
   });
