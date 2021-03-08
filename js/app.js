6a2390ed6dbf7702584aa57fa316ccd85220c883
Sam Atkinson <sammodeste@yahoo.com>
2021-03-08 22:16:40 +0000

    toggle button changes to dark  colour theme

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
-const btn = document.querySelector(".switch");
 
-
-
-document.querySelector('[data-switch-dark]').addEventListener('click', function() {
+document.querySelector('[data-switch-dark]').addEventListener('change', function() {
     document.body.classList.toggle('dark');
     
-    
-       if (btn.textContent === "Dark Theme") {
-         
-            btn.textContent = "Light Theme";
-       } else {
-            btn.textContent = "Dark Theme";
-       }
   });
+
+
