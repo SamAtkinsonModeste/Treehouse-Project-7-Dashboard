d23d3f01ee03cc717ebbbf89c28443d10378a7fb
Sam Atkinson <sammodeste@yahoo.com>
2021-03-08 22:43:36 +0000

    delay colour theme changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 
-document.querySelector('[data-switch-dark]').addEventListener('change', function() {
-    document.body.classList.toggle('dark');
+ document.querySelector('[data-switch-dark]').addEventListener('change', function() {
+
+var delayInMilliseconds = 900; //1 second
+
+setTimeout(function() {
+  //your code to be executed after 1 second
+ 
+     document.body.classList.toggle('dark');
+     
+  
+}, delayInMilliseconds);
+
+});
+// document.querySelector('[data-switch-dark]').addEventListener('change', function() {
+//     document.body.classList.toggle('dark');
     
-  });
+//   });
 
 
