afe4ae9907017da4e3ddbe814f365a5a88c3dc44
Sam Atkinson <sammodeste@yahoo.com>
2021-03-08 23:39:30 +0000

    tried to fix bug on ball text 4 iPhone

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
+// const ballText = document.querySelector(".ball-text");
 
+// const toggleText = () => {
+    
+//      if (ballText.textContent !== "Off") {
+//          ballText.textContent = "On";
+//      }
+// };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  document.querySelector('[data-switch-dark]').addEventListener('change', function() {
 
-var delayInMilliseconds = 900; //1 second
+const delayTime = 900; //1 second
 
 setTimeout(function() {
-  //your code to be executed after 1 second
- 
+  //code to be executed after the time value set to delayTime runs
+     
      document.body.classList.toggle('dark');
      
   
-}, delayInMilliseconds);
+}, delayTime);
 
 });
 // document.querySelector('[data-switch-dark]').addEventListener('change', function() {
